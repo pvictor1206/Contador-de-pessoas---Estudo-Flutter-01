92998daa79a610d3708bf55c618cf362cb4ee29a
pvictor1206 <paulomagalhaes1206@gmail.com>
2021-04-30 22:32:06 -0300

    Aula 02

--- comtador_de_pessoas/lib/main.dart
+++ comtador_de_pessoas/lib/main.dart
@@ -1,11 +1,26 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'core/app_widget.dart';
 
 void main() {
-  runApp(MaterialApp( //widget
-    title: "Contador de Pessoas",
-    home: Container(
-     color: Colors.white,
-    )
-  ));
+  runApp(MaterialApp(
+      //widget
+      title: "Contador de Pessoas",
+      // Colunas verticais de informações
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Alinhamento do eixo principal (centro da tela)
+        children: <Widget>[
+          Text(
+            "Pessoas: 0",
+            style:
+                TextStyle(color: Colors.lightBlue, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Pode Entrar!",
+            style:
+            TextStyle(color: Colors.lightBlue, fontStyle: FontStyle.italic,
+            fontSize: 30.0),
+          )
+        ],
+      )));
 }
